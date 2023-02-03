f64666d0fa80f027922d846b8b441947a1b529ea
Robert <robert.aleks.zuk@gmail.com>
2023-02-03 13:07:15 +0100

    initila 3

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useEffect, useContext } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { Outlet } from "react-router-dom";
+import { getProducts } from "../../features/products/productSlice";
 
 function Dashboard() {
+  const dispatch = useDispatch();
+
+  const searchValue = "";
+
+  const {
+    products,
+    isLoading: productsIsLoading,
+    isError: productsIsError,
+    message: productsMessage,
+  } = useSelector((state) => state.products);
+
+  useEffect(() => {
+    dispatch(getProducts(searchValue ? searchValue : ""));
+  }, []);
+
+  console.log(products);
+
   return (
     <div>
       <div>Dashboard</div>
